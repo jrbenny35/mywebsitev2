89ab54226989904ed67b5e1882b939d86b534c87
jrbenny35 <bennyjr169@gmail.com>
2015-10-02 11:06:11 -0400

    Set up gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,20 +1,31 @@
 /**
  * Created by root on 10/2/15.
  */
+'use strict';
 
 module.exports = function(grunt){
 
-    grunt.initconfig({
+    grunt.initConfig({
 
         pkg: grunt.file.readJSON('package.json'),
-        sass :{
-            index:{
-                files{
-                    
+        sass: {
+            index: {
+                files: {
+                    'public/stylesheets/main.css': 'public/stylesheets/main.scss'
+
                 }
             }
         }
 
+
     });
+    //Load grunt tasks
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    //Grunt tasks
+    grunt.registerTask('build',['sass']);
+    grunt.registerTask('default',['build']);
+
 
 };
