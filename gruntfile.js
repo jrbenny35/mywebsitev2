982104db2cd7dfc8d9ea02025942331378ad5f84
jrbenny35 <bennyjr169@gmail.com>
2015-10-02 22:52:18 -0400

    Configured grunt to minify

--- gruntfile.js
+++ gruntfile.js
@@ -11,9 +11,45 @@ module.exports = function(grunt){
         sass: {
             index: {
                 files: {
-                    'public/stylesheets/main.css': 'public/stylesheets/main.scss'
+                    'public/dist/stylesheets/main.css': 'public/stylesheets/main.scss'
 
                 }
+            }
+        },
+        watch:{
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'public/dist/stylesheets',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'public/dist/stylesheets',
+                    ext: '.min.css'
+                }]
+            }
+        },
+        uglify :{
+            options :{
+                mangle: false
+            },
+            index:{
+                files :{
+                    'public/dist/javascripts/indexFinal.min.js' : 'public/dist/javascripts/indexFinal.js'
+                }
+            }
+        },
+        concat:{
+            index:{
+                src:[
+                    'public/javascripts/app.js',
+                    'public/javascripts/controllers.js'
+                ],
+                dest:'public/dist/javascripts/indexFinal.js'
             }
         }
 
@@ -22,9 +58,13 @@ module.exports = function(grunt){
     //Load grunt tasks
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     //Grunt tasks
-    grunt.registerTask('build',['sass']);
+    grunt.registerTask('build',['sass', 'cssmin', 'concat', 'uglify']);
     grunt.registerTask('default',['build']);
 
 
